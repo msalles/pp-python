fde5e30b7fe86098d2f8d0634c5a457a1f16d28a
Marcel Salles <msalles@users.noreply.github.com>
2023-11-15 13:13:15 -0600

    cambios en student3

--- OOP/student3.py
+++ OOP/student3.py
@@ -4,13 +4,14 @@
 
 
 class Student:
-    def __init__(self, name, house):
+    def __init__(self, name, house, patronus):
         if not name:
             raise ValueError("Missing Name!!!")
         if house not in (["Griffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]):
             raise ValueError("Invalid House!")
         self.name = name
         self.house = house
+        self.patronus = patronus
 
     def __str__(self):
         return f"{self.name} from {self.house}"
@@ -23,7 +24,8 @@ def main():
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    return Student(name, house)
+    patronus = input("Patronus: ")
+    return Student(name, house, patronus)
 
 
 if __name__ == "__main__":
